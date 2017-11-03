d0e74a8d02b1e7e76e095f1aa1cea05f2f6bade1
Flávio Caetano <flavio@vieiracaetano.com>
2017-11-03 21:40:05 -0200

    Fix: Running Fastlane with a single device

---
+++ fastlane/actions/codecov.rb
@@ -0,0 +1,1 @@
+codecov/codecov.rb
--- pre-push.sh
+++ pre-push.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if $(which bundle &> /dev/null); then
-	bundle exec fastlane test
+	bundle exec fastlane ci
 elif $(which fastlane &> /dev/null); then
-	fastlane test
+	fastlane ci
 else
 	echo 'Fastlane not installed; Run `bundle install` or install Fastlane directly'
 	exit 1
